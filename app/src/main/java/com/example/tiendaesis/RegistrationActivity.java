91c7abba8584925a7305c9c0448dd5dfefc27b3a
bryam1023 <bryvelasquezg@upt.pe>
2021-11-30 05:15:13 -0500

    SISTEMA LOGIN CON FIREBASE TERMINADO

--- app/src/main/java/com/example/tiendaesis/RegistrationActivity.java
+++ app/src/main/java/com/example/tiendaesis/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.tiendaesis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,17 +10,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText name,email,password;
-
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
         getSupportActionBar().hide();
-
+        auth = FirebaseAuth.getInstance();
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
@@ -35,10 +41,32 @@ public class RegistrationActivity extends AppCompatActivity {
 
         if(TextUtils.isEmpty(userName)){
             Toast.makeText(this,"Enter Name!",Toast.LENGTH_SHORT).show();
+
         }
         if(TextUtils.isEmpty(userEmail)){
             Toast.makeText(this,"Enter Email Address!",Toast.LENGTH_SHORT).show();
+            return ;
         }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Enter Password!",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        if(userPassword.length()<6){
+            Toast.makeText(this,"Enter Password!",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull  Task<AuthResult> task) {
+                         if(task.isSuccessful()){
+                             Toast.makeText(RegistrationActivity.this, "Succesfully Register", Toast.LENGTH_SHORT).show();
+                             startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                         } else{
+                             Toast.makeText(RegistrationActivity.this, "Registration  Failed"+task.getException(), Toast.LENGTH_SHORT).show();
+                         }
+                    }
+                });
     }
 
     public void singin(View view) {
