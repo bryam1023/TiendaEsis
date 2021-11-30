8c7c80f61e5c57cebfeb0cbdc58f877e7c180ee3
bryam1023 <bryvelasquezg@upt.pe>
2021-11-30 06:05:46 -0500

    avance de commit  de sistema

---
+++ app/src/main/java/com/example/tiendaesis/OnBoardingActivity.java
@@ -0,0 +1,26 @@
+package com.example.tiendaesis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    SliderAdapter sliderAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+
+        viewPager =findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+
+        //call adapter
+        sliderAdapter = new SliderAdapter( this);
+        viewPager.setAdapter(sliderAdapter);
+    }
+}
